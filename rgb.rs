2d3337f4d4a84131fa471a8892a6a14556e92c6c
MarcinRycek <156324310+MarcinRycek@users.noreply.github.com>
2025-05-13 23:56:15 +0200

    RGB colors in rust rgb.rs

---
+++ rgb.rs
@@ -0,0 +1,82 @@
+use core::fmt;
+
+#[derive(Debug, PartialEq, PartialOrd)]
+struct Rgb {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Rgb {
+    fn from_3u8(r: u8, g: u8, b: u8) -> Self {
+        Rgb { r, g, b }
+    }
+
+    fn from_3percent(r: f64, g: f64, b: f64) -> Result<Self, String> {
+        if !(0.0..=100.0).contains(&r) || !(0.0..=100.0).contains(&g) || !(0.0..=100.0).contains(&b) {
+            return Err("Error: values must be: 0.0 - 100.0".to_string());
+        }
+        Ok(Rgb {
+            r: (255.0 * r / 100.0).round() as u8,
+            g: (255.0 * g / 100.0).round() as u8,
+            b: (255.0 * b / 100.0).round() as u8,
+        })
+    }
+
+    fn gray(n: f64) -> Result<Self, String> {
+        Self::from_3percent(n, n, n)
+    }
+
+    fn white() -> Self {
+        Rgb { r: 255, g: 255, b: 255 }
+    }
+
+    fn black() -> Self {
+        Rgb { r: 0, g: 0, b: 0 }
+    }
+
+    fn invert(&mut self) {
+        self.r = 255 - self.r;
+        self.g = 255 - self.g;
+        self.b = 255 - self.b;
+    }
+
+    fn intensity(&self) -> f64 {
+        (self.r as f64 + self.g as f64 + self.b as f64) / (255.0 * 3.0)
+    }
+
+    fn as_rgb_u8tuple(&self) -> (u8, u8, u8) {
+        (self.r, self.g, self.b)
+    }
+
+    fn as_cmy_u8tuple(&self) -> (u8, u8, u8) {
+        (255 - self.r, 255 - self.g, 255 - self.b)
+    }
+}
+
+impl fmt::Display for Rgb {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Rgb({}, {}, {})", self.r, self.g, self.b)
+    }
+}
+
+fn main() {
+    let szary1 = Rgb::from_3u8(127, 127, 127);
+    let szary2 = Rgb::from_3percent(50.0, 50.0, 50.0).unwrap();
+    let szary3 = Rgb::gray(50.0).unwrap();
+    let fiolet = Rgb::from_3u8(100, 35, 120);
+    let bialy1 = Rgb::white();
+    let bialy2 = Rgb::from_3u8(255, 255, 255);
+    let mut czarny1 = Rgb::black();
+    let czarny2 = Rgb::from_3u8(0, 0, 0);
+
+    println!("{} {}", szary1 == szary2, szary1 == szary3);
+    println!("{} {}", bialy1 == bialy2, czarny1 == czarny2);
+
+    czarny1.invert();
+    println!("{}", bialy1 == czarny1);
+
+    println!("{:.5}", fiolet.intensity() == 1.0 / 3.0);
+    println!("{}", fiolet.as_rgb_u8tuple() == (100, 35, 120));
+    println!("{}", fiolet.as_cmy_u8tuple() == (155, 220, 135));
+}
